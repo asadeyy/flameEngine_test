51f737dab28f23f81203a3c53d82c2f652e93f52
asadeyy <asada.kanna.819@s.kyushu-u.ac.jp>
2022-03-03 15:28:17 +0900

    NewTutorialStart

--- moonlander/android/app/src/main/kotlin/com/example/moonlander/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moonlander
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ tutorial/android/app/src/main/kotlin/com/example/tutrial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tutrial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
